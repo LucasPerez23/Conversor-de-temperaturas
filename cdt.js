1b479f2587f85bd98659ffe614e5e971f93b35ba
LucasPerez23 <53823875+LucasPerez23@users.noreply.github.com>
2019-08-07 11:06:47 -0300

    Subo tp

---
+++ cdt.js
@@ -0,0 +1,13 @@
+function fartocel (form)
+	{form.celsi.value = (form.fc.value-32)/1.8 }
+function fartokel (form)
+	{form.kelv.value = (form.fk.value-32)/1.8+273.15 }
+function keltofar (form)
+	{form.fah2.value = (form.kf.value-273.15)*1.8+32 }
+function keltocel (form)
+	{form.celsi2.value = form.kc.value-273.15 }
+function celtofar (form)
+	{form.fah.value = form.cf.value*1.8+32}
+function celtokel (form)
+	{form.kelv2.value = (form.ck.value*1)+273.15
+	}	
